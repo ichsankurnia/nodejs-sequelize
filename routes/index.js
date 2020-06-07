e94f3879242387c1bfa190aec7a14ca6f86fc01f
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-06-08 02:04:12 +0700

    add middlewares validate authenticate

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const { Router } = require('express');
+
+// import middleware
+const isAuthenticated = require('../app/middlewares/verify-token')
+const auth = require('../app/middlewares/authenticate');
+// import controller
 const controllerUser = require('../app/controllers/user')
 
 const router = Router();
@@ -8,7 +13,9 @@ router.get('/', (req, res) => res.status(200).json(
     { author: 'Ichsan Kurniawan', contact: 'kurniaichsan45@gmail.com', description: 'Rest API with nodejs express sequelize postgre' }
 ));
 
+router.post('/auth', auth.login);
+
 // user
-router.get('/user', controllerUser.getAllData);
+router.get('/user', isAuthenticated, controllerUser.getAllData);
 
 module.exports = router;
