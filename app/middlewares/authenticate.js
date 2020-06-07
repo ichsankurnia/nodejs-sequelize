c6e2d66387ccc31a07649f4501cf0b7f3b2b4ae3
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-06-08 02:03:00 +0700

    add middlewares to validate authenticate

---
+++ app/middlewares/authenticate.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const models = require('../../database/models')
+
+const login = async (req, res) => {
+    try {
+        const {username, password, remember_boo} = req.body
+        const data = await models.User.findOne({where: {username_var: username, password_var: password}})
+
+        if(data){
+            
+            const token = jwt.sign({ 
+                user_id: data.user_id,  
+                username_var: data.username_var,
+                email_var: data.email_var
+            }, process.env.JWT_KEY, { expiresIn: '7d' });
+
+            // jika user memilih remember me untuk data login nya
+            if (remember_boo === true) {
+                // simpan dan update token ke table user
+                await models.User.update({
+                        token_text: token,
+                        updated_at: new Date()
+                    }, { where: { user_id: data.user_id }
+                });
+            }
+
+            return res.status(200).json({ code: 0, message: 'success authenticate', data: {token: token} });
+        }else{
+            return res.status(200).json({ code: 1, message: 'wrong username or password', data: null });
+        }
+    } catch (error) {
+        return res.status(200).send({code:1, message: error.message, data: null})
+    }
+}
+
+module.exports = {login}
