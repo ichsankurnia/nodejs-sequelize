cd5d04a792629014530a02b13e208d017b03261c
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-06-11 19:49:27 +0700

    add controller-post to route and multer configuration to upload file

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const multer = require('multer');
 
 // import middleware
 const isAuthenticated = require('../app/middlewares/verify-token')
 const auth = require('../app/middlewares/authenticate');
 // import controller
 const controllerUser = require('../app/controllers/controller-user')
+const controllerPost = require('../app/controllers/controller-post')
 
 const router = Router();
 
@@ -22,5 +24,25 @@ router.post('/user',                             controllerUser.createData)
 router.put('/user/:id',         isAuthenticated, controllerUser.updateData)
 router.delete('/user/:id',      isAuthenticated, controllerUser.deleteData)
 router.delete('/truncate-user', isAuthenticated, controllerUser.truncateData)
+
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './tmp/')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+// var upload = multer({ dest: './public/assets/img/upload/' })
+// var upload = multer({storage: storage}).single('file');     // file is req name
+var upload = multer({storage: storage})
+
+// post
+router.get('/post',             isAuthenticated, controllerPost.getAllPost)
+router.get('/post/:id',         isAuthenticated, controllerPost.getPostById)
+router.post('/new-post',        isAuthenticated, controllerPost.createNewPost)
+router.post('/post',            isAuthenticated, upload.single('file'), controllerPost.createPost)
 
 module.exports = router;
