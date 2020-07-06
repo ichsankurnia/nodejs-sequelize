9ff5400a70a1e0467286d0a6fd1d0e30d13fa8f0
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-07-06 15:55:13 +0700

    add paramter is_login to allow user login

--- app/middlewares/authenticate.js
+++ app/middlewares/authenticate.js
@@ -11,22 +11,32 @@ const login = async (req, res) => {
             const dataByPass = await models.User.findOne({where: {username_var: username_var, password_var: password_var}})
             
             if(dataByPass){
-                const token = jwt.sign({ 
-                    user_id: data.user_id,  
-                    username_var: data.username_var,
-                    email_var: data.email_var
-                }, process.env.JWT_KEY, { expiresIn: '1d' });
-    
-                // jika user memilih remember me untuk data login nya
-                if (remember_boo === true) {
-                    // simpan dan update token ke table user
-                    await models.User.update({
-                            token_text: token,
-                            updated_at: new Date()
-                        }, { where: { user_id: data.user_id }
-                    });
+                const login = await models.User.findOne({
+                    where: {username_var: username_var, password_var: password_var, is_login: true},
+                    attributes : ['username_var', 'password_var', 'email_var']
+                })
+
+                if(login){
+                    const token = jwt.sign({ 
+                        user_id: data.user_id,  
+                        username_var: data.username_var,
+                        email_var: data.email_var,
+                        is_login: true
+                    }, process.env.JWT_KEY, { expiresIn: '1d' });
+        
+                    // jika user memilih remember me untuk data login nya
+                    if (remember_boo === true) {
+                        // simpan dan update token ke table user
+                        await models.User.update({
+                                token_text: token,
+                                updated_at: new Date()
+                            }, { where: { user_id: data.user_id }
+                        });
+                    }
+                    return res.json({ code: 0, message: 'success authenticate', data: login, token: token });
+                }else{
+                    return res.json({ code: 1, message: 'your account is not activate, please check your email to verify and activate account', data: null });
                 }
-                return res.json({ code: 0, message: 'success authenticate', data: {token: token} });
             }else{
                 return res.json({ code: 1, message: 'wrong password', data: null });
             }
