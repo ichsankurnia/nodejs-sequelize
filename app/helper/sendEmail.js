a455212e8ba21db8a41ec6b60650c7e42b63ca3b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-07-06 15:54:48 +0700

    enable image send with template html nodemailer

--- app/helper/sendEmail.js
+++ app/helper/sendEmail.js
@@ -16,14 +16,42 @@ const config = {
 
 const sendMailRegister = async (params) => {
 
-    const template = await fs.readFileSync('./public/views/register.html', 'utf8');
+
+
+    const template = await fs.readFileSync('./public/views/test1.html', 'utf8');
     var transporter = await nodemailer.createTransport(config);
 
     var mailOptions = {
         from: '"GoesToNobel" info@goes2nobel.com',
-        to: params.email_var,
+        to: params.email,
         subject: '[Node Email] - Success Registration',
         html: mustache.render(template, params),
+        attachments:[
+            {
+                filename : 'h1.gif',
+                path: path.join(__dirname + './../../public/views/images/h1.gif'),
+                cid : 'h1'
+            },
+            {
+                filename : 'fb.gif',
+                path: path.join(__dirname + './../../public/views/images/fb.gif'),
+                cid : 'fb'
+            },
+            {
+                filename : 'tw.gif',
+                path: path.join(__dirname + './../../public/views/images/tw.gif'),
+                cid : 'tw'
+            },
+            {
+                filename : 'left.gif',
+                path: path.join(__dirname + './../../public/views/images/left.gif'),
+                cid : 'left'
+            },{
+                filename : 'right.gif',
+                path: path.join(__dirname + './../../public/views/images/right.gif'),
+                cid : 'right'
+            },
+        ]
         // html: `<h4>Your Account Has Been Registered!</h4>`,
         // text: "Your Account Has Been Registered!"
     };
@@ -54,7 +82,7 @@ const sendMailRegister2 = async (params) => {
     };
     
     var transporter = await nodemailer.createTransport(config);
-    const path_file = path.join(__dirname + './../../public/views/register.html')
+    const path_file = path.join(__dirname + './../../public/views/test1.html')
 
     readHTMLFile(path_file, function(err, html) {
         var template = handlebars.compile(html);
@@ -62,7 +90,7 @@ const sendMailRegister2 = async (params) => {
 
         var mailOptions = {
             from: '"GoesToNobel" info@goes2nobel.com',
-            to: params.email_var,
+            to: params.email,
             subject : 'test subject',
             html : htmlToSend
          };
